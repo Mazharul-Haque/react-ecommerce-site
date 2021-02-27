78a7e766f922a406fd34bedb248eb89a537a324e
Mazharul-Haque <mshagor009@gmail.com>
2021-02-27 12:14:41 +0600

    Order Summary & Total Price Add

--- src/components/Header/Shop/Shop.js
+++ src/components/Header/Shop/Shop.js
@@ -1,12 +1,21 @@
 import React, { useState } from 'react';
 import fakeData from '../../../fakeData'
+import Cart from '../../Cart/Cart';
 import Product from '../../Product/Product';
 import './Shop.css'
 
 
 const Shop = () => {
-    const first10 = fakeData.slice(0, 10);
+    const first10 = fakeData.slice(0, 20);
     const [products, setProducts] = useState(first10);
+    const [cart, setCart] = useState([]);
+
+
+    const handleAddProduct = (product) =>{
+        const newCart = [...cart, product];
+        setCart(newCart);
+        //console.log("add product");
+    }
 
 
     return (
@@ -14,14 +23,18 @@ const Shop = () => {
             <div className="product-container">
         
                 {
-                    products.map(pd => <Product product={pd} ></Product>)
+                    products.map(pd => <Product
+                       handleAddProduct = {handleAddProduct}
+                         product={pd} 
+                         ></Product>)
                 }
                 
 
             </div>
             <div className="cart-container">
-                <h3>This is cart</h3>
+                <Cart cart={cart} ></Cart>
 
+          
             </div>
 
 
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -4,7 +4,7 @@ import { faCoffee, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css'
 
 const Product = (props) => {
-    console.log(props.product);
+    //console.log(props);
     const { img, name, seller, price, stock } = props.product;
     return (
         <div className="product" >
@@ -17,7 +17,10 @@ const Product = (props) => {
                 <p> <small>by :{seller} </small> </p>
                 <p className="price">${price} </p>
                 <p><small>only {stock} left in the stock - order soon</small></p>
-                <button className="cart-button"><FontAwesomeIcon icon={faShoppingCart} />add to cart</button>
+                <button 
+                className="cart-button"
+                onClick={() => props.handleAddProduct(props.product)}
+                ><FontAwesomeIcon icon={faShoppingCart}/>add to cart</button>
             </div>
 
 
